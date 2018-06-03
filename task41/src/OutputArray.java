e5646013de43de407e50eeeea24cfdad91743bee
ttsrg <39533021+ttsrg@users.noreply.github.com>
2018-06-04 00:11:06 +0300

    Add files via upload

---
+++ task41/src/OutputArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class OutputArray {
+    static void output(int arrayI, int i) {
+
+        i++;
+        //return new int[4];
+        System.out.print("value:" + i + "=" + arrayI + "\t|\t");
+    }
+    //System.out.print(Arrays.toString(output()));  ??????
+
+}
+
